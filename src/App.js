f3ad565ce9e80036677c192e8b9098531c0029a6
revathypr <revathypr75@gmail.com>
2021-12-16 16:03:35 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Helloworld from "./component/screens/Helloworld";
+
+class App extends React.Component{
+  render(){
+    return (
+        <>
+           <Helloworld/>
+           <Helloworld/>
+        </>
+    )
+  }
+}
+export default App
+
+
